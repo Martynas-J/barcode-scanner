9add7ab93f2ee0516bb36182fc5f7eb618938118
Martynas-J <aurimhas@gmail.com>
2024-06-10 10:52:11 +0300

    add style v1.2

--- src/app/scanner/page.jsx
+++ src/app/scanner/page.jsx
@@ -56,7 +56,7 @@ const ScannerPage = () => {
             <ButtonComponent onClick={() => minusHandler(scannedCode)}>Išimti</ButtonComponent>
             <ButtonComponent onClick={() => addNewHandler(scannedCode)}>Nauja</ButtonComponent>
           </div>
-          <Link className="flex justify-end bg-gray-800 border-2 shadow-xl border-gray-800 text-white py-3 px-6 rounded-md font-bold  hover:bg-white hover:text-gray-800  focus:outline-none  focus:bg-white focus:text-gray-800 w-70px" href="/#">Atgal</Link>
+          <Link className=" bg-gray-800 border-2 shadow-xl border-gray-800 text-white py-3 px-6 rounded-md font-bold  hover:bg-white hover:text-gray-800  focus:outline-none  focus:bg-white focus:text-gray-800 " href="/#">Atgal</Link>
         </div>
       )}
       {error && (
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,5 +1,6 @@
 "use client"
 
+import Link from 'next/link';
 import React, { useState } from 'react';
 
 const Form = ({ onSubmit }) => {
@@ -64,7 +65,9 @@ const Form = ({ onSubmit }) => {
       <button type="submit" className={buttonClass}>
         Pridėti
       </button>
+      <Link className="flex bg-gray-800 border-2 shadow-xl border-gray-800 text-white py-3 px-6 rounded-md font-bold  hover:bg-white hover:text-gray-800  focus:outline-none  focus:bg-white focus:text-gray-800 " href="/#">Atgal</Link>
     </form>
+
   );
 };
 
