af91fd4df9a9f1e67d3b12b04b5bc2163389ecd5
Martynas-J <Aurimhas@gmail.com>
2024-06-16 18:08:17 +0300

    add  materials and inventory

--- src/app/materials/page.jsx
+++ src/app/materials/page.jsx
@@ -1,41 +1,42 @@
 "use client";
-
+import DataTable from "@/components/DataTable";
 import Link from "next/link";
-
+import Loading from "@/components/Loading/Loading";
+import { FromDb } from "@/Functions/simpleFunctions";
 import { signOut, useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
-export default function Home() {
+export default function Materials() {
   const { status } = useSession();
   const router = useRouter();
-
+  const { result, isLoading, mutate } = FromDb(`getResults`);
   useEffect(() => {
     if (status === "unauthenticated") {
       router?.push("/dashboard/login");
     }
   }, [status, router]);
 
+  if (isLoading) {
+    return <Loading />;
+  }
+
   return (
     <main className="flex  flex-col items-center gap-5 pt-5">
-      <h1 className="text-2xl font-bold">Galimos veiklos</h1>
+      <h1 className="text-2xl font-bold">Esamos prekės</h1>
+
+      <DataTable data={result} />
       <Link
         className=" text-xl border rounded-lg bg-gray-300 px-4 py-2 hover:bg-gray-500"
-        href="/materials"
+        href="/scanner"
       >
-        Medžiagos
+        Skenuoti
       </Link>
-      <Link
-        className=" text-xl border rounded-lg bg-gray-300 px-4 py-2 hover:bg-gray-500"
-        href="/inventory"
-      >
-        Inventorius
-      </Link>
-      {status === "authenticated" && (
-        <button onClick={signOut} className="hover:text-red-500 pt-5">
-          Atsijungti
-        </button>
-      )}
+      <div className="mt-5">
+        <Link className="text-center" href="/#">
+          Atgal
+        </Link>
+      </div>
     </main>
   );
 }
