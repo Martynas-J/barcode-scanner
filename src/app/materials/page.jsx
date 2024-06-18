89497bd38ae61df4c76cab897b48449e267fa20d
Martynas-J <aurimhas@gmail.com>
2024-06-18 08:00:02 +0300

    add search input v1.2

--- src/app/materials/page.jsx
+++ src/app/materials/page.jsx
@@ -12,22 +12,37 @@ export default function Materials() {
   const { status } = useSession();
   const router = useRouter();
   const { result, isLoading, mutate } = FromDb(`getResults`);
+  const [filteredResult, setFilteredResult] = useState([]);
+
   useEffect(() => {
     if (status === "unauthenticated") {
       router?.push("/dashboard/login");
     }
   }, [status, router]);
 
+  useEffect(() => {
+    if (result) {
+      setFilteredResult(result);
+    }
+  }, [result]);
+
   if (isLoading) {
     return <Loading />;
   }
-  const handleSearch = () => {
-    console.log('Searching for:', searchQuery);
-};
+  const handleSearch = (searchQuery) => {
+    const query = searchQuery.toLowerCase();
+    const filtered = result.filter(item => 
+      Object.values(item).some(value => 
+        value.toString().toLowerCase().includes(query)
+      )
+    );
+    setFilteredResult(filtered);
+  };
+
   return (
     <main className="flex  flex-col items-center gap-5 pt-5">
       <h1 className="text-2xl font-bold">Esamos prekės</h1>
-      <SearchForm searchHandler={handleSearch}/>
+      <SearchForm handleSearch={handleSearch}/>
       <DataTable data={result} />
       <Link
         className=" text-xl border rounded-lg bg-gray-300 px-4 py-2 hover:bg-gray-500"
