f0c1ec1619d346113d4f844a3721a9bbeef9269e
Martynas-J <aurimhas@gmail.com>
2024-06-19 13:14:25 +0300

    add route getStat v3,2

--- src/app/statistics/page.jsx
+++ src/app/statistics/page.jsx
@@ -1,5 +1,6 @@
 "use client";
 import { FromDb } from '@/Functions/simpleFunctions';
+import { STATHEADER } from '@/config/config';
 import React from 'react'
 
 const Statistics = () => {
@@ -8,7 +9,47 @@ const Statistics = () => {
   return (
     <div className="container mx-auto text-center">
     <h1 className="text-2xl font-bold">Statistika</h1>
-    
+    <div className="overflow-x-auto">
+      <table className="min-w-full bg-white dark:bg-gray-800">
+        <thead>
+          <tr>
+            {STATHEADER.map((header) => (
+              <th
+                key={header}
+                className="py-2 px-2 border-b-2 border-gray-200 dark:border-gray-600 bg-gray-100 text-center dark:bg-gray-700  text-xs font-semibold text-gray-700 dark:text-gray-300 uppercase tracking-wider"
+              >
+                {header}
+              </th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {result?.map((item) => (
+            <tr
+              key={item._id}
+              className={`even:bg-gray-50 dark:even:bg-gray-700 odd:bg-white dark:odd:bg-gray-800`}
+            >
+              {Object.values(item)
+                .slice(1, 5)
+                .map((value, index) => (
+                  <td
+                    key={index}
+                    className="py-2 px-2 border-b text-center border-gray-200 dark:border-gray-600 text-gray-700 dark:text-gray-300"
+                  >
+                    {index === 2 ? (
+                      <span
+                        className={`${value < 3 && "text-red-500 font-bold"}`}
+                      >{`${value} vnt.`}</span>
+                    ) : (
+                      value
+                    )}
+                  </td>
+                ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   </div>
   )
 }
--- src/config/config.js
+++ src/config/config.js
@@ -1,3 +1,4 @@
 export const API_URL = "https://barcode-scanner-omega.vercel.app";
 //export const API_URL = "http://localhost:3000"
 export  const TABLEHEADER = ['Spauzdintuvas', 'Modelis', 'Likutis'];
+export  const STATHEADER = ['Vardas', 'Kiekis', 'Veiksmas', 'Data'];
