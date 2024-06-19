25e8cfa19edb74f17a43627eb0b7eb75acfc672b
Martynas-J <aurimhas@gmail.com>
2024-06-19 15:28:37 +0300

    fix scan button to top

--- src/app/materials/page.jsx
+++ src/app/materials/page.jsx
@@ -3,7 +3,7 @@ import DataTable from "@/components/DataTable";
 import Link from "next/link";
 import Loading from "@/components/Loading/Loading";
 import { FromDb } from "@/Functions/simpleFunctions";
-import {  useSession } from "next-auth/react";
+import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 import SearchForm from "@/components/SearchForm";
@@ -31,8 +31,8 @@ export default function Materials() {
   }
   const handleSearch = (searchQuery) => {
     const query = searchQuery.toLowerCase();
-    const filtered = result.filter(item => 
-      Object.values(item).some(value => 
+    const filtered = result.filter(item =>
+      Object.values(item).some(value =>
         value.toString().toLowerCase().includes(query)
       )
     );
@@ -42,19 +42,23 @@ export default function Materials() {
   return (
     <main className="flex  flex-col items-center gap-5 pt-5">
       <h1 className="text-2xl font-bold">Esamos prekės <Link href="/statistics">📈</Link></h1>
-      <SearchForm handleSearch={handleSearch}/>
-      <DataTable data={filteredResult} />
-      <Link
-        className=" text-xl border rounded-lg bg-gray-300 px-4 py-2 hover:bg-gray-500"
-        href="/scanner"
-      >
-        Skenuoti
-      </Link>
-      <div className="mt-5">
-        <Link className="text-center" href="/#">
-          Atgal
+      <SearchForm handleSearch={handleSearch} />
+      <div className="flex">
+        <Link
+          className=" text-xl border rounded-lg bg-gray-300 px-4 py-2 hover:bg-gray-500"
+          href="/scanner"
+        >
+          Skenuoti
         </Link>
+        <DataTable data={filteredResult} />
+
+        <div className="mt-5">
+          <Link className="text-center" href="/#">
+            Atgal
+          </Link>
+        </div>
       </div>
+
     </main>
   );
 }
