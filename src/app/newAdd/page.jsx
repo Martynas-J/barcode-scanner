5d74f6842f8874e95565f9ca3a78c35f8cd0bb44
Martynas-J <aurimhas@gmail.com>
2024-06-19 13:04:44 +0300

    add route getStat v3,1

--- src/app/newAdd/page.jsx
+++ src/app/newAdd/page.jsx
@@ -28,13 +28,14 @@ const NewAdd = () => {
 
     let action = "Nauja";
     let count = data.value;
+    const existingItem = result?.find(item => item.code === parsedData.code);
 
     if (parsedData) {
-      console.log("Count" +count);
-      console.log("result" +result);
+      console.log("Count" + count);
+      console.log("result" + existingItem);
 
-      const sum = data.value - result.itemValue;
-      console.log("Sum" +sum);
+      const sum = data.value - existingItem.itemValue;
+      console.log("Sum" + sum);
       action = sum > 0 ? "Pridėta" : sum < 0 ? "Išimta" : "Redaguota";
       count = sum;
     }
