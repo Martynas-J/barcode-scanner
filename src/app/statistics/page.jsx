1d24d515c434a0586d05d8c70eac91c2ffac8b8f
Martynas-J <aurimhas@gmail.com>
2024-06-19 10:48:37 +0300

    add route getStat v1.4

--- src/app/statistics/page.jsx
+++ src/app/statistics/page.jsx
@@ -4,6 +4,7 @@ import React from 'react'
 
 const Statistics = () => {
     const { result, isLoading, mutate } = FromDb(`getStatistics`);
+    console.log(result)
   return (
     <div>Statistics</div>
   )
